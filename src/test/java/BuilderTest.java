33c9cf467fdd0425628d052b71df123b29c08d83
re0078 <reza.abdollahzadeh12@gmail.com>
2023-05-11 21:29:44 +0330

    test: added BuilderTest

---
+++ src/test/java/BuilderTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.example.builderpattern.ShoppingOrderBuilder;
+import org.example.builderpattern.ShoppingOrder;
+
+public class BuilderTest {
+    @Test
+    public void testBuildShoppingOrder() {
+        ShoppingOrderBuilder builder = new ShoppingOrderBuilder();
+        ShoppingOrder order = builder.setCustomerName("Reza Abdz")
+                .addItem("Shirt", 2)
+                .addItem("Pants", 1)
+                .setShippingAddress("Azadi Ave., Tehran")
+                .build();
+
+        Assertions.assertEquals("Reza Abdz", order.getCustomerName());
+        Assertions.assertEquals(2, order.getItemQuantity("Shirt"));
+        Assertions.assertEquals(1, order.getItemQuantity("Pants"));
+        Assertions.assertEquals(0, order.getItemQuantity("Books"));
+        Assertions.assertEquals("Azadi Ave., Tehran", order.getShippingAddress());
+    }
+
+}
